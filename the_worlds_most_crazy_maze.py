0504b828de1d6b3acfb5baf46879d08b34181824
hnaicker_cpt <hnaicker@student.wethinkcode.co.za>
2020-11-19 18:06:22 +0200

    inital commit

---
+++ the_worlds_most_crazy_maze.py
@@ -0,0 +1,15 @@
+def create_random_obstacles(): 
+    pass
+
+def is_position_blocked(x, y):
+    # return obstacle_present
+    pass
+def is_path_blocked(x1, y1, x2, y2):
+    # return path_is_blocked
+    pass
+def get_obstacles():
+    pass
+    # return obstacles
+
+def print_obstacles(obstacles):
+    pass  
